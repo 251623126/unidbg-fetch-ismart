7b47674d049ecc6fb0d9c60c90106a914ee0ba29
251623126 <251623126@qq.com>
2024-01-13 16:43:10 +0800

    first commit

--- src/main/java/com/ismart/controller/ISmartController.java
+++ src/main/java/com/ismart/controller/ISmartController.java
@@ -4,7 +4,7 @@ package com.ismart.controller;
 import com.ismart.service.ISmartService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
